8daef3ad7d28cd6d3ae8f44a73f43ca12e5b9cb2
Maxim Abramov <bigballs@cerf7.xyz>
2022-04-13 07:30:45 +0300

    Add blueprint for REST API

---
+++ cerf7/api.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, current_app
+from cerf7.db import db
+
+
+bp = Blueprint("api", __name__, url_prefix="/api")
+
+
+@bp.route("/get_user_chats", methods=["POST"])
+def get_user_chats():
+    pass
+
+
+@bp.route("load_chat_messages", methods=["POST"])
+def load_chat_messages():
+    pass
