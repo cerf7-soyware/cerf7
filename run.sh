fe2e55106cec30a7540376f103e0bc56f8484786
Maxim Abramov <bigballs@cerf7.xyz>
2022-04-13 14:43:27 +0300

    Split command in multiple lines

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-gunicorn -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1 --log-level=debug --access-logfile - --error-logfile - "cerf7:create_app()"
+gunicorn -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w "$1" --log-level=debug \
+  --access-logfile - --error-logfile - "cerf7:create_app()"
